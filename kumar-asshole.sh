dd85297d68b3673df3e55d6d408de922610108ab
marrakchino <nabilelqatib@gmail.com>
2017-05-17 00:31:13 +0200

    Start kumar-asshole

--- kumar-asshole.sh
+++ kumar-asshole.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # Requires kumar_asshole script in your bin
 #
 
-exec kumar_asshole
+# Exit early if no sessions with my username are found
+if ! who | grep -wq $USER; then
+  exit
+
+# First log in, then check for any unread mails from kumar containing the keywords. 
+# If so, reply with something like: 
+# Database fixes 
+# 
+# No problem. I've fixed it. 
+#
+# Please be careful next time.
